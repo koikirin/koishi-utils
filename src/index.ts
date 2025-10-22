edfc792013547782869206dff92a21b4f2717ea1
Hieuzest <girkirin@hotmail.com>
2025-10-23 02:43:53 +0800

    feat: escape everything with backslash

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export function splitBackslashEscapedArgs(input: string, delimiter: RegExp = /\s|,|\|\|/): string[] {
-  const regex = new RegExp(/\\[\s\\]|/.source + delimiter.source)
+  const regex = new RegExp(/\\[\s\S]|/.source + delimiter.source)
   const args: string[] = []
   let startIndex = 0, lastIndex = 0
   let match: RegExpExecArray | null
@@ -8,7 +8,7 @@ export function splitBackslashEscapedArgs(input: string, delimiter: RegExp = /\s
     const [fullMatch] = match
 
     if (!fullMatch.startsWith('\\')) {
-      const arg = input.slice(startIndex, lastIndex + match.index).replace(/\\([\s\\])/g, '$1')
+      const arg = input.slice(startIndex, lastIndex + match.index).replace(/\\([\s\S])/g, '$1')
       startIndex = lastIndex + match.index + fullMatch.length
 
       if (arg) args.push(arg)
@@ -16,7 +16,7 @@ export function splitBackslashEscapedArgs(input: string, delimiter: RegExp = /\s
     lastIndex += match.index + fullMatch.length
   }
 
-  const lastArg = input.slice(startIndex).replace(/\\([\s\\])/g, '$1')
+  const lastArg = input.slice(startIndex).replace(/\\([\s\S])/g, '$1')
   if (lastArg) args.push(lastArg)
   return args
 }
